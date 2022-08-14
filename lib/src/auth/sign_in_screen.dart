0dfe24ac418e90d2e5fc09396d0a015f614a4a76
Felipe Santos <dev.felipesantos@gmail.com>
2022-08-14 13:29:25 -0300

    feat: create sign in screen

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+    );
+  }
+}
