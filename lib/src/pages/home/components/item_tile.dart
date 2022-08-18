8a7fb181649d84f8de065462ed0c79e88f086c81
Felipe Santos <dev.felipesantos@gmail.com>
2022-08-18 20:01:41 -0300

    feat: products cards

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -2,9 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:store/src/config/custom_colors.dart';
+import 'package:store/src/models/item_model.dart';
 
 class ItemTile extends StatelessWidget {
-  const ItemTile({Key? key}) : super(key: key);
+  final ItemModel item;
+
+  const ItemTile({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +21,47 @@ class ItemTile extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
       ),
-      child: Column(
-        children: [
-          // Imagem
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Imagem
+            Expanded(
+              child: Image.asset(item.imgUrl),
+            ),
 
-          // Nome
+            // Nome
+            Text(
+              item.itemName,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
 
-          // Preço
-        ],
+            // Preço
+            Row(
+              children: [
+                Text(
+                  item.price.toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: CustomColors.customSwatchColor),
+                ),
+                Text(
+                  '/${item.unit}',
+                  style: TextStyle(
+                    color: Colors.grey.shade500,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
