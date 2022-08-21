ed50b1af55fcbbcb087bda797ecc8bd38614f27f
Felipe Santos <dev.felipesantos@gmail.com>
2022-08-21 14:00:04 -0300

    feat: order status dot

--- lib/src/pages/orders/components/order_status_widget.dart
+++ lib/src/pages/orders/components/order_status_widget.dart
@@ -5,7 +5,18 @@ class OrderStatusWidget extends StatelessWidget {
   final String status;
   final bool isOverdue;
 
-  const OrderStatusWidget({
+  final Map<String, int> allStatus = <String, int>{
+    'pending_payment': 0,
+    'refunded': 1,
+    'paid': 2,
+    'preparing_purchase': 3,
+    'shipping': 4,
+    'delivered': 5,
+  };
+
+  int get currentStatus => allStatus[status]!;
+
+  OrderStatusWidget({
     Key? key,
     required this.isOverdue,
     required this.status,
@@ -14,16 +25,41 @@ class OrderStatusWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _StatusDot(
-          title: 'Pagamento efetuado',
+        const _StatusDot(
           isActive: true,
+          title: 'Pedido Confirmado',
         ),
-        _StatusDot(
-          title: 'Aguardando pagamento',
-          isActive: false,
-        )
+        const _CustomDivider(),
+        if (currentStatus == 1) ...[
+          const _StatusDot(
+            isActive: true,
+            title: 'Estornado',
+            backgroundColor: Colors.orange,
+          ),
+        ] else if (isOverdue) ...[
+          const _StatusDot(
+            isActive: true,
+            title: 'Pagamento expirado',
+            backgroundColor: Colors.red,
+          )
+        ]
       ],
+    );
+  }
+}
+
+class _CustomDivider extends StatelessWidget {
+  const _CustomDivider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 9, vertical: 3),
+      height: 10,
+      width: 2,
+      color: Colors.grey.shade300,
     );
   }
 }
@@ -31,11 +67,13 @@ class OrderStatusWidget extends StatelessWidget {
 class _StatusDot extends StatelessWidget {
   final bool isActive;
   final String title;
+  final Color? backgroundColor;
 
   const _StatusDot({
     Key? key,
     required this.isActive,
     required this.title,
+    this.backgroundColor,
   }) : super(key: key);
 
   @override
@@ -51,8 +89,9 @@ class _StatusDot extends StatelessWidget {
             border: Border.all(
               color: CustomColors.customSwatchColor,
             ),
-            color:
-                isActive ? CustomColors.customSwatchColor : Colors.transparent,
+            color: isActive
+                ? backgroundColor ?? CustomColors.customSwatchColor
+                : Colors.transparent,
           ),
           child: isActive
               ? const Icon(
@@ -64,7 +103,12 @@ class _StatusDot extends StatelessWidget {
         ),
         const SizedBox(width: 5),
         Expanded(
-          child: Text(title),
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 12,
+            ),
+          ),
         )
       ],
     );
